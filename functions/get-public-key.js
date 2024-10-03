9f7526833e0ea6c6fbc03def79c7923308f9f4fe
Diadema-DTI <dti.diadema@gmail.com>
2024-10-03 02:14:40 -0300

    Update get-public-key.js

--- functions/get-public-key.js
+++ functions/get-public-key.js
@@ -1,6 +1,22 @@
 const crypto = require('crypto');
 
-exports.handler = async () => {
+exports.handler = async (event, context) => {
+  // Set CORS headers
+  const headers = {
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Headers': 'Content-Type',
+    'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
+  };
+
+  // Handle preflight requests
+  if (event.httpMethod === 'OPTIONS') {
+    return {
+      statusCode: 200,
+      headers,
+      body: '',
+    };
+  }
+
   const { publicKey, privateKey } = crypto.generateKeyPairSync('rsa', {
     modulusLength: 2048,
   });
